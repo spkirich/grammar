47db02611c7fdd0ff7ceecff7f4eafd18d0f47ff
Sergey Kirichenko <spkirich@gmail.com>
2022-10-26 00:47:10 +0300

    Add eliminateNonGeneratingNonterminals function

--- src/Grammar.hs
+++ src/Grammar.hs
@@ -3,6 +3,14 @@
 module Grammar
   ( Grammar (..)
 
+  -- * Nonterminals
+
+  , generatingNonterminals
+
+  -- * Normalization
+
+  , eliminateNonGeneratingNonterminals
+
   ) where
 
 import           Data.Set   (Set)
@@ -26,3 +34,15 @@ data Grammar t n = Grammar
     -- ^ The start nonterminal
 
   } deriving (Show)
+
+-- | A set of all generating nonterminals of the grammar
+generatingNonterminals :: (Ord t, Ord n) => Grammar t n -> Set n
+generatingNonterminals g = fst $ until (uncurry (==)) (\(_, ns) -> (ns, step ns)) (Set.empty, basis) where
+  step ns = Set.union ns . Set.map lhs . Set.filter (all (`Set.member` ns) . rhsNonterminals) $ productions g
+  basis = Set.map lhs . Set.filter (null . rhsNonterminals) $ productions g
+
+-- | Eliminate all non-generating nonterminals of the grammar
+eliminateNonGeneratingNonterminals :: (Ord t, Ord n) => Grammar t n -> Grammar t n
+eliminateNonGeneratingNonterminals g = g { nonterminals = gns, productions = gps } where
+  gps = Set.filter (\p -> Set.member (lhs p) gns && all (`Set.member` gns) (rhsNonterminals p)) $ productions g
+  gns = generatingNonterminals g
--- test/Spec.hs
+++ test/Spec.hs
@@ -14,6 +14,16 @@ instance (Arbitrary t, Arbitrary n) => Arbitrary (Production t n) where
   -- | An arbitrary production
   arbitrary = Production <$> arbitrary <*> arbitrary
 
+instance (Arbitrary t, Ord t, Arbitrary n, Ord n) => Arbitrary (Grammar t n) where
+
+  -- | An arbitrary grammar
+  arbitrary = Grammar <$> arbitrary <*> arbitrary <*> arbitrary <*> arbitrary
+
+-- | Test elimination of non-generating nonterminals efficiency
+prop_eliminateNonGeneratingNonterminalsEffective :: Grammar Char Int -> Bool
+prop_eliminateNonGeneratingNonterminalsEffective g
+  = nonterminals (eliminateNonGeneratingNonterminals g) == generatingNonterminals g
+
 return []
 runTests = $quickCheckAll
 
