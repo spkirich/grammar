bb510866706e01ac011cbe2cd6b2ff6895fc1ec1
Sergey Kirichenko <spkirich@gmail.com>
2022-10-26 00:10:44 +0300

    Fix minor blots

--- src/Production.hs
+++ src/Production.hs
@@ -3,7 +3,7 @@
 module Production
   ( Production (..)
 
-    -- * Right hand side
+    -- * Right-hand side
 
   , rhsTerminals
   , rhsNonterminals
@@ -19,22 +19,22 @@ import qualified Data.Set as Set
 data Production t n = Production
 
   { lhs :: n
-    -- ^ A nonterminal on the left hand side
+    -- ^ A nonterminal on the left-hand side
 
   , rhs :: [Either t n]
-    -- ^ A sequence of symbols on the right hand side
+    -- ^ A sequence of symbols on the right-hand side
 
   } deriving (Eq, Ord, Show)
 
--- | A set of symbols on the right hand side of a production
+-- | A set of symbols on the right-hand side of a production
 rhsSymbols :: (Ord t, Ord n) => Production t n -> (Set t, Set n)
 rhsSymbols (Production _ r) = (Set.fromList ts, Set.fromList ns) where
   (ts, ns) = partitionEithers r
 
--- | A set of terminals on the right hand side of a production
+-- | A set of terminals on the right-hand side of a production
 rhsTerminals :: (Ord t, Ord n) => Production t n -> Set t
 rhsTerminals = fst . rhsSymbols
 
--- | A set of nonterminals on the right hand side of a production
+-- | A set of nonterminals on the right-hand side of a production
 rhsNonterminals :: (Ord t, Ord n) => Production t n -> Set n
 rhsNonterminals = snd . rhsSymbols
