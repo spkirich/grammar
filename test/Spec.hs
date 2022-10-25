859cbf1ea220b7cc598308a065bd895a99d75d9a
Sergey Kirichenko <spkirich@gmail.com>
2022-10-25 23:56:55 +0300

    Remove the strange test

--- test/Spec.hs
+++ test/Spec.hs
@@ -14,21 +14,6 @@ instance (Arbitrary t, Arbitrary n) => Arbitrary (Production t n) where
   -- | An arbitrary production
   arbitrary = Production <$> arbitrary <*> arbitrary
 
--- | Test production right hand side partitionability
-prop_productionRhsPartitionable :: Production Char Int -> Bool
-prop_productionRhsPartitionable p = testTs && testNs
-  where
-    testTs = if all isRight $ productionRhs p
-      then       null ts
-      else not $ null ts
-
-    testNs = if all isLeft  $ productionRhs p
-      then       null ns
-      else not $ null ns
-
-    ts = productionRhsTerminals    p
-    ns = productionRhsNonterminals p
-
 return []
 runTests = $quickCheckAll
 
